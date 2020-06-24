c1f143b15e9773153b8b41773e97fe318aa28a24
James Sutton <1892175+zeryx@users.noreply.github.com>
2020-06-24 16:54:32 -0500

    validating corrections

--- src/ImageClassifierDemo.py
+++ src/ImageClassifierDemo.py
@@ -83,7 +83,6 @@ def calculate_topn_accuracy(results):
     accuracy /= len(results)
     return accuracy
 
-
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
 def apply(input):
